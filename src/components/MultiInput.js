936ed95c79cedebc0fd8a98c0e04785cc148d635
Szép Arnold <szep.arnold@gmail.com>
2018-07-15 17:02:25 +0200

    done?

--- src/components/MultiInput.js
+++ src/components/MultiInput.js
@@ -1,63 +1,64 @@
 import React, { Component } from 'react';
-import InputItem from './InputItem';
+import PropTypes from 'prop-types';
+import closeIcon from '../assets/close.svg';
 
 class MultiInput extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      inputArray: [],
-      inputs: []
-    };
+  save() {
+    var sendArray = this.props.inputArray;
+    this.props.onChange(sendArray);
   }
 
-  componentWillMount() {
-    this.setState({
-      inputArray: ['first', 'second', '']
-    });
+  focus(ev) {
+    ev.target.parentNode.classList.add('active');
   }
 
-  update(ev, key) {
-    var inputArray = this.state.inputArray;
-
-    inputArray[key] = ev.target.value;
-
-    if (inputArray.length === key + 1) {
-      inputArray.push('');
-    }
-
-    this.setState({ inputArray: inputArray });
-    //console.log('update', key, ev);
+  blur(ev) {
+    ev.target.parentNode.classList.remove('active');
   }
 
-  remove(key) {
-    var inputArray = this.state.inputArray;
-    inputArray.splice(key, 1);
-    
-    this.setState({ inputArray: inputArray });
-  }
-  
   render() {
-    var inputs = this.state.inputArray.map((item, key) => {
+    var inputs = this.props.inputArray.map((item, key, array) => {
       return (
         <div className="input_wrap" key={key}>
-          {key}
-          <input type="text" value={item} onChange={(ev) => this.update(ev, key)} placeholder="test" />
-          <button onClick={() => this.remove(key)}>X</button>
-          <br />
+          <label htmlFor={'input' + key}>{'Input #' + key}</label>
+          <input type="text" id={'input' + key} value={item} onChange={(ev) => this.props.update(ev, key)} onBlur={(ev) => this.blur(ev)} onFocus={(ev) => this.focus(ev)} placeholder="empty" />
+          <button onClick={() => this.props.remove(key)} style={{ display: array.length === key + 1 ? 'none' : '' }}>
+            <img src={closeIcon} alt="" />
+          </button>
         </div>
       );
     });
 
     return (
       <section className="multi_input">
-        <h1>Multi Input</h1>
-        {inputs}
+        <aside className="multi_input-sidebar">
+          <h1>Multi Input</h1>
+          <p>
+            Heirloom tumblr glossier jianbing godard put a bird on it hammock shabby chic retro cardigan sartorial hell of meditation.
+          </p>
+        </aside>
 
-        <button>Cancel</button>
-        <button>Save</button>
+        <div className="multi_input-inputs">
+          {inputs}
+        </div>
+
+        <div className="cl"></div>
+
+        <footer className="multi_input-footer">
+          <button onClick={this.props.reset.bind(this)}>Reset</button>
+          <button onClick={this.save.bind(this)}>Save</button>
+        </footer>
       </section>
     );
   }
+}
+
+MultiInput.propTypes = {
+  inputArray: PropTypes.array,
+  update: PropTypes.func,
+  reset: PropTypes.func,
+  remove: PropTypes.func,
+  onChange: PropTypes.func
 }
 
 export default MultiInput;
--- src/index.js
+++ src/index.js
@@ -5,21 +5,91 @@ import './index.css';
 import logo from './assets/logo.svg';
 import MultiInput from './components/MultiInput';
 
-//import registerServiceWorker from './registerServiceWorker';
-
 class App extends React.Component {
-    render() {        
+    constructor() {
+        super();
+
+        this.startState = ['first', 'second', ''];
+        this.savedItems = '';
+
+        this.state = {
+            savedArray: [],
+            inputArray: []
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            inputArray: this.startState.slice()
+        });
+    }
+
+    save(array) {
+        var arrayCopy = array.slice();
+        if (arrayCopy[arrayCopy.length - 1] === '') {
+            arrayCopy.splice(arrayCopy.length - 1, 1);
+        }
+
+        this.setState({
+            savedArray: arrayCopy
+        });
+    }
+
+    update(ev, key) {
+        var inputArray = this.state.inputArray;
+
+        inputArray[key] = ev.target.value;
+
+        if (inputArray.length === key + 1) {
+            inputArray.push('');
+        }
+
+        this.setState({ inputArray: inputArray });
+    }
+
+    remove(key) {
+        var inputArray = this.state.inputArray;
+        inputArray.splice(key, 1);
+
+        this.setState({ inputArray: inputArray });
+    }
+
+    reset(array) {
+        this.setState({
+            inputArray: this.startState.slice()
+        });
+    }
+
+    render() {
+        this.savedItems = this.state.savedArray.map((item, key) => {
+            return (
+                <li key={key}>
+                    {item}
+                </li>
+            );
+        });
+
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" width="113" height="80" />
+            <div className="page_wrap">
+                <header className="header">
+                    <img src={logo} className="header-logo" alt="logo" width="113" height="80" />
                 </header>
 
-                <MultiInput />
+                <div className="container">
+                    <MultiInput
+                        inputArray={this.state.inputArray}
+                        update={this.update.bind(this)}
+                        reset={this.reset.bind(this)}
+                        remove={this.remove.bind(this)}
+                        onChange={this.save.bind(this)} />
+
+                    <ol>
+                        {this.savedItems}
+                    </ol>
+                </div>
             </div>
         );
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
